7ad3bf2d5d24c24d5a83bc6e0f772999f4404eca
lifishake <lifishake@gmail.com>
2019-01-17 17:01:22 +0800

    主要修改margin

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -29,5 +29,7 @@
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		?>
 	</header><!-- .entry-header -->
-
+    <div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-content -->
 </article><!-- #post-## -->
