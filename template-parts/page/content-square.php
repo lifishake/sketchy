975e163d9a31af27deaed97af9a2d7aa29104692
lifishake <lifishake@163.com>
2022-07-30 22:26:36 +0800

    增加方形支持

---
+++ template-parts/page/content-square.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Sketchy
+ * @since 1.0
+ * @version 1.0
+ */
+global $page_type;
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class( "page-no-edge" ); ?>>
+	<header class="entry-header">
+	<?php
+		the_title( '<h1 class="screen-reader-text">', '</h1>' ); 
+	?>
+	</header><!-- .entry-header -->
+	<div class="entry-content">
+		<?php
+		if('album' == $page_type) {
+			bddb_the_gallery($page_type);
+		}else{
+			the_content();
+		}
+		?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
