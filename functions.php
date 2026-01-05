97e21c9dd17e5f12abc2371c11a44d05cdd40929
gao_wangzhong <gao_wangzhong@outlook.com>
2026-01-05 23:17:50 +0800

    恢复unveil，放弃浏览器版lazyload

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ if ( version_compare( $GLOBALS['wp_version'], '4.7-alpha', '<' ) ) {
 	return;
 }
 
-define('SKETCHY_VER', '20251229');
+define('SKETCHY_VER', '20251230');
 
 function sketchy_setup() {
 
